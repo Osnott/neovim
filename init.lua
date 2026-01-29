5ffccd84cab5a3fd22383cfa4be48bc7ed7444d9
osnott <hi@osnott.xyz>
2026-01-28 18:39:01 -0700

    fix(plug): fix treesitter configuration

--- init.lua
+++ init.lua
@@ -52,16 +52,14 @@ require "nvim-autopairs".setup()
 require "nvim-treesitter.configs".setup({
 	ensure_installed = { "tsx", "c", "zig", "lua", "markdown_inline", "markdown", "python", "vim", "vimdoc", "nix" },
 	auto_install = true,
-	modules = {
-		highlight = {
-			enable = true
-		},
-		indent = {
-			enable = true
-		},
-		incremental_selection = {
-			enable = true
-		}
+	highlight = {
+		enable = true
+	},
+	indent = {
+		enable = true
+	},
+	incremental_selection = {
+		enable = true
 	}
 })
 
@@ -103,7 +101,7 @@ vim.lsp.config('tinymist', {
 	}
 })
 
-vim.lsp.enable({ "lua_ls", "tinymist", "vtsls", "basedpyright", "nixd", "zls" })
+vim.lsp.enable({ "lua_ls", "tinymist", "vtsls", "basedpyright", "nixd", "zls", "cssls" })
 
 -- treesitter
 
@@ -113,7 +111,7 @@ vim.api.nvim_create_autocmd("FileType", {
 		local hasStarted = pcall(vim.treesitter.start)
 
 		-- indent
-		local noIndent = { "tsx" }
+		local noIndent = {  }
 		if hasStarted and not vim.list_contains(noIndent, ctx.match) then
 			vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 		end
